4d2001ff68c50069d34e384fb0352bf7a0b4ab87
Camille Joy De Villa <devillacamillejoym@gmail.com>
2022-06-22 07:05:36 +0800

    add components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import Logo from "./img/logo.png";
 import Avatar from "./img/avatar.png";
 import { MdShoppingBasket } from "react-icons/md";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="w-screen fixed z-50 p-6 px-16">
       {/* Desktop and Tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
-        <div className="flex items-center gap-2">
+        <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
           <p className="text-headingColor text-xl font-bold">City</p>
-        </div>
+        </Link>
 
         <div className="flex items-center gap-8 ">
           <ul className="flex items-center gap-8">
@@ -36,9 +38,10 @@ const Header = () => {
             </div>
           </div>
 
-          <img
+          <motion.img
+            whileTap={{ scale: 0.6 }}
             src={Avatar}
-            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl"
+            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer"
             alt="userProfile"
           />
         </div>
