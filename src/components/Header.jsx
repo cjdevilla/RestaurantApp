f6aba1bd5d4ad3c64b6137e94420ef7358dbd7aa
Camille Joy De Villa <devillacamillejoym@gmail.com>
2022-06-27 07:00:30 +0800

    add utils and update component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Logo from "../img/logo.png";
 import Avatar from "../img/avatar.png";
-import { MdShoppingBasket } from "react-icons/md";
+import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -16,14 +16,16 @@ const Header = () => {
   const [{ user }, dispatch] = useStateValue();
 
   const login = async () => {
-    const {
-      user: { refreshToken, providerData },
-    } = await signInWithPopup(firebaseAuth, provider);
-    dispatch({
-      type: actionType.SET_USER,
-      user: providerData[0],
-    });
-    localStorage.setItem("user", JSON.stringify(providerData[0]));
+    if (!user) {
+      const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    }
   };
 
   return (
@@ -66,6 +68,14 @@ const Header = () => {
               alt="userProfile"
               onClick={login}
             />
+            <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all  duration-100 ease-in-out text-textColor text-base">
+                New Item <MdAdd />
+              </p>
+              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all  duration-100 ease-in-out text-textColor text-base">
+                Logout <MdLogout />
+              </p>
+            </div>
           </div>
         </div>
       </div>
