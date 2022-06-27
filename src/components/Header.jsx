821247a4a9d7dfc6c3d3273aa28e5fbb007f706e
Camille Joy De Villa <devillacamillejoym@gmail.com>
2022-06-28 06:56:35 +0800

    update component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../img/logo.png";
 import Avatar from "../img/avatar.png";
 import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
@@ -15,6 +15,8 @@ const Header = () => {
 
   const [{ user }, dispatch] = useStateValue();
 
+  const [isMenu, setIsMenu] = useState(false);
+
   const login = async () => {
     if (!user) {
       const {
@@ -25,6 +27,8 @@ const Header = () => {
         user: providerData[0],
       });
       localStorage.setItem("user", JSON.stringify(providerData[0]));
+    } else {
+      setIsMenu(!isMenu);
     }
   };
 
@@ -68,14 +72,20 @@ const Header = () => {
               alt="userProfile"
               onClick={login}
             />
-            <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
-              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all  duration-100 ease-in-out text-textColor text-base">
-                New Item <MdAdd />
-              </p>
-              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all  duration-100 ease-in-out text-textColor text-base">
-                Logout <MdLogout />
-              </p>
-            </div>
+            {isMenu && (
+              <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+                {user && user.email === "devillacamillejoym@gmail.com" && (
+                  <Link to="/createItem">
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all  duration-100 ease-in-out text-textColor text-base">
+                      New Item <MdAdd />
+                    </p>
+                  </Link>
+                )}
+                <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all  duration-100 ease-in-out text-textColor text-base">
+                  Logout <MdLogout />
+                </p>
+              </div>
+            )}
           </div>
         </div>
       </div>
