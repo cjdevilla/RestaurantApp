291cff4dd57905a2b8f5ef50e5dc32f4bb00c57b
Camille Joy De Villa <devillacamillejoym@gmail.com>
2022-07-01 07:00:29 +0800

    update MainContainer

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,7 +4,7 @@ import Delivery from "../img/delivery.png";
 const MainContainer = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2 ">
-      <div className="py-2 flex-1 flex flex-col items-start justify-center md:items-center">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">
             Bike Delivery
@@ -16,6 +16,24 @@ const MainContainer = () => {
               alt="delivery"
             />
           </div>
+        </div>
+        <div>
+          <p className="text-[2.5rem] font-bold tracking-wide text-headingColor">
+            The Fastest Delivery in
+            <span className="text-orange-600 text-[3rem]"> Your City</span>
+          </p>
+          <p className="text-base text-textColor text-center md:text-left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <button
+            type="button"
+            className="bg-gradient-to-br from-orange-400 to-orange-500 w-full px-4 py-2 rounded-lg mt-3  hover:shadow-lg transition-all ease-in-out duration-100 md:w-auto"
+          >
+            Order Now
+          </button>
         </div>
       </div>
       <div className="p-2 bg-blue-400 flex-1"></div>
